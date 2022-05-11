4d155f72a9db3c443c1a85a260df63b388ca30ab
VioletYaoegg <violetyaoegg@outlook.com>
2022-05-12 01:40:37 +0800

    most add,edit,delete finished

--- src/views/AdminStudent.vue
+++ src/views/AdminStudent.vue
@@ -28,7 +28,111 @@
           prop="gpa"
           label="绩点">
       </el-table-column>
+      <el-table-column label="操作" width = "150">
+        <template slot-scope="scope">
+          <el-button
+              size="mini"
+              @click="handleEdit(scope.row), dialogVisible = true">编辑</el-button>
+          <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+
+      </el-table-column>
     </el-table>
+
+<!--    编辑学生信息-->
+    <el-dialog
+        title="编辑学生信息"
+        :visible.sync="dialogVisible"
+        width="30%">
+
+      <el-input
+          style="height: 50px"
+          placeholder="学号"
+          v-model="sno"
+          :disabled="true">
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="姓名"
+          v-model="sname"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="性别"
+          v-model="sex"
+          clearable>
+      </el-input>
+
+      <el-input
+          style="height: 50px"
+          placeholder="学院编号"
+          v-model="dno"
+          clearable>
+      </el-input>
+
+      <el-input
+          style="height: 50px"
+          placeholder="绩点"
+          v-model="gpa"
+          clearable>
+      </el-input>
+<!--      也可以不修改绩点，毕竟绩点是算出来的-->
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取消</el-button>
+      <el-button type="primary" @click="handleEdit2() ,dialogVisible = false">确定</el-button>
+
+    </span>
+    </el-dialog>
+
+<!--    添加学生-->
+    <el-dialog
+        title="添加学生信息"
+        :visible.sync="dialogVisible2"
+        width="30%">
+
+      <el-input
+          style="height: 50px"
+          placeholder="学号"
+          v-model="sno"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="姓名"
+          v-model="sname"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="性别"
+          v-model="sex"
+          clearable>
+      </el-input>
+
+      <el-input
+          style="height: 50px"
+          placeholder="学院编号"
+          v-model="dno"
+          clearable>
+      </el-input>
+
+
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible2 = false">取消</el-button>
+      <el-button type="primary" @click="handleAdd() ,dialogVisible2 = false">确定</el-button>
+
+    </span>
+    </el-dialog>
+
+    <el-button
+        style="margin-top: 50px"
+        size="mini"
+        type="success"
+        @click="dialogVisible2=true">添加</el-button>
   </div>
 </template>
 
@@ -42,9 +146,107 @@ export default {
 
     })
   },
+methods:{
+  handleEdit(row){
+    this.sno = row.sno
+    this.sname = row.sname
+    this.sex = row.sex
+    this.dno = row.dno
+    this.department = row.department
+  },
+  handleEdit2(){
+    let _this = this;
+    axios.post("http://localhost:9090/student/updateStudent",
+        {
+          sno: _this.sno,
+          sname: _this.sname,
+          sex: _this.sex,
+          dno: _this.dno,
+          gpa: _this.gpa
+        }
+    ).then(function (resp){
+      if(resp.data){
+        _this.$alert('修改成功','提示',{
+          confirmButtonText : '确定',
+          callback : action => {
+            location.reload();
+          }
+        });
+      }
+      else{
+        _this.$alert('修改失败','提示',{
+          confirmButtonText : '确定'
+        });
+      }
+    })
+  },
+  handleDelete(row){
+    let _this = this
+    this.$confirm('是否删除学生 '+row.sno +':'+row.sname+'?', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    }).then(() => {
+      axios.get("http://localhost:9090/student/delete/" + row.sno).then(function (resp){
+        if(resp.data){
+          _this.$alert('已删除','',{
+            confirmButtonText : '确定',
+            callback : action => {
+              location.reload();
+            }
+          });
+        }
+      });
+    }).catch(() => {
+      this.$message({
+        type: 'info',
+        message: '已取消删除'
+      });
+    });
+  },
+
+  handleAdd(){
+    let _this = this;
+    axios.post("http://localhost:9090/student/add",
+        {
+          sno: _this.sno,
+          sname: _this.sname,
+          sex: _this.sex,
+          dno: _this.dno,
+          gpa:0,
+          password:123456
+        }
+    ).then(function (resp){
+      if(resp.data){
+        _this.$alert('添加成功','提示',{
+          confirmButtonText : '确定',
+          callback : action => {
+            location.reload();
+          }
+        });
+      }
+      else{
+        _this.$alert('添加失败','提示',{
+          confirmButtonText : '确定'
+        });
+      }
+    })
+  },
+},
+
+
   data() {
     return {
-      tableData: []
+      tableData: [],
+      dialogVisible:false,
+      dialogVisible2: false,
+      sno:'',
+      sname:'',
+      sex:'',
+      dno:'',
+      department:'',
+      gpa:'',
+      password:''
     }
   }
 }
--- src/views/AdminTeacher.vue
+++ src/views/AdminTeacher.vue
@@ -24,7 +24,104 @@
           label="学院"
           width="180">
       </el-table-column>
+      <el-table-column label="操作" width = "150">
+        <template slot-scope="scope">
+          <el-button
+              size="mini"
+              @click="handleEdit(scope.row), dialogVisible = true">编辑</el-button>
+          <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+
+      </el-table-column>
     </el-table>
+
+    <!--    编辑教师信息-->
+    <el-dialog
+        title="编辑教师信息"
+        :visible.sync="dialogVisible"
+        width="30%">
+
+      <el-input
+          style="height: 50px"
+          placeholder="工号"
+          v-model="tno"
+          :disabled="true">
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="姓名"
+          v-model="tname"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="性别"
+          v-model="sex"
+          clearable>
+      </el-input>
+
+      <el-input
+          style="height: 50px"
+          placeholder="学院编号"
+          v-model="dno"
+          clearable>
+      </el-input>
+
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取消</el-button>
+      <el-button type="primary" @click="handleEdit2() ,dialogVisible = false">确定</el-button>
+
+    </span>
+    </el-dialog>
+
+    <!--    添加教师-->
+    <el-dialog
+        title="添加教师信息"
+        :visible.sync="dialogVisible2"
+        width="30%">
+
+      <el-input
+          style="height: 50px"
+          placeholder="工号"
+          v-model="tno"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="教师姓名"
+          v-model="tname"
+          clearable>
+      </el-input>
+      <el-input
+          style="height: 50px"
+          placeholder="性别"
+          v-model="sex"
+          clearable>
+      </el-input>
+
+      <el-input
+          style="height: 50px"
+          placeholder="学院编号"
+          v-model="dno"
+          clearable>
+      </el-input>
+
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible2 = false">取消</el-button>
+      <el-button type="primary" @click="handleAdd() ,dialogVisible2 = false">确定</el-button>
+
+    </span>
+    </el-dialog>
+
+
+    <el-button
+        style="margin-top: 50px"
+        size="mini"
+        type="success"
+        @click="dialogVisible2=true">添加</el-button>
   </div>
 </template>
 
@@ -37,9 +134,103 @@ export default {
       _this.tableData = resp.data;
     })
   },
+  methods:{
+    handleEdit(row){
+      this.tno = row.tno
+      this.tname = row.tname
+      this.sex = row.sex
+      this.dno = row.dno
+      this.department = row.department
+    },
+    handleEdit2(){
+      let _this = this;
+      axios.post("http://localhost:9090/teacher/updateTeacher",
+          {
+            tno: _this.tno,
+            tname: _this.tname,
+            sex: _this.sex,
+            dno: _this.dno
+          }
+      ).then(function (resp){
+        if(resp.data){
+          _this.$alert('修改成功','提示',{
+            confirmButtonText : '确定',
+            callback : action => {
+              location.reload();
+            }
+          });
+        }
+        else{
+          _this.$alert('修改失败','提示',{
+            confirmButtonText : '确定'
+          });
+        }
+      })
+    },
+
+    handleDelete(row){
+      let _this = this
+      this.$confirm('是否删除老师 '+row.tno +':'+row.tname+'?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        axios.get("http://localhost:9090/teacher/delete/" + row.tno).then(function (resp){
+          if(resp.data){
+            _this.$alert('已删除','',{
+              confirmButtonText : '确定',
+              callback : action => {
+                location.reload();
+              }
+            });
+          }
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+
+    handleAdd(){
+      let _this = this;
+      axios.post("http://localhost:9090/teacher/add",
+          {
+            tno: _this.tno,
+            tname: _this.tname,
+            sex: _this.sex,
+            dno: _this.dno,
+            password:123456
+          }
+      ).then(function (resp){
+        if(resp.data){
+          _this.$alert('添加成功','提示',{
+            confirmButtonText : '确定',
+            callback : action => {
+              location.reload();
+            }
+          });
+        }
+        else{
+          _this.$alert('添加失败','提示',{
+            confirmButtonText : '确定'
+          });
+        }
+      })
+    }
+  },
   data() {
     return {
-      tableData: []
+      tableData: [],
+      dialogVisible:false,
+      dialogVisible2:false,
+      tno:'',
+      tname:'',
+      sex:'',
+      dno:'',
+      department:'',
+      password:''
     }
   }
 }
